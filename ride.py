f2487680ecffd118bf00987f6b6368c73550c108
mp51 <mpeczk@gmail.com>
2018-03-01 19:32:25 +0100

    init fix

--- ride.py
+++ ride.py
@@ -8,7 +8,7 @@ class Ride:
     completed = False
     distance = None
 
-    def __int__(self, a, b, x, y, s, f):
+    def __init__(self, a, b, x, y, s, f):
         self.a = a
         self.b = b
         self.x = x
@@ -16,3 +16,4 @@ class Ride:
         self.s = s
         self.f = f
         self.distance = abs(a-x) + abs(b-y)
+
