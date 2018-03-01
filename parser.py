01048b60d75874b541bb1eafc9984f15f5d19ec6
Mikołaj Kamiński <mkaminski321@gmail.com>
2018-03-01 20:07:15 +0100

    Parser 1.0 is ready

--- parser.py
+++ parser.py
@@ -1,11 +1,21 @@
+from car import Car
+from ride import Ride
+
 class Parser:
 
     def __init__(self):
         pass
 
-    def ParseInput(self, f):
-        p = [list(map(int, line.split(" "))) for line in open("./a_example.in").read().split("\n") if ]
-        print(p[0])
+    @staticmethod
+    def ParseInput(f):
+        p = [list(map(int, line.split(" "))) for line in open("./data/" + f).read().split("\n") if line.__len__()!=0]
+        
+        R = p[0][0]
+        C = p[0][1]
+        B = p[0][4]
+        T = p[0][5]
 
-p = Parser()
-p.ParseInput("./a_example.in")
+        F = [Car() for i in range(p[0][2])]
+        N = [Ride(r[0], r[1], r[2], r[3], r[4], r[5]) for i,r in enumerate(p) if i>0]
+
+        return R, C, F, N, B, T
