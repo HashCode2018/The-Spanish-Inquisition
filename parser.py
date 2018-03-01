638a3d7b9701ff8ce44f868e2f7b4a5e874adfa1
Mikołaj Kamiński <mkaminski321@gmail.com>
2018-03-01 19:55:19 +0100

    Especially for Pawel

---
+++ parser.py
@@ -0,0 +1,11 @@
+class Parser:
+
+    def __init__(self):
+        pass
+
+    def ParseInput(self, f):
+        p = [list(map(int, line.split(" "))) for line in open("./a_example.in").read().split("\n") if ]
+        print(p[0])
+
+p = Parser()
+p.ParseInput("./a_example.in")
